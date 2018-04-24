26c9a2e6f3ef3941fb1078368f8930e6b8f36e28
kdncode <kdncode@gmail.com>
2018-04-23 22:51:39 -0500

    finished post request from db

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require('mongoose')
 var app = express();
+
+mongoose.connect("mongodb://localhost/gocamping");
 
 app.use(bodyParser.urlencoded( { extended: true} ));
 
 app.set("view engine", "ejs")
 
-var camps = [
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
-    { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"}
-    // { name: "Hiiiii", image: "https://img.sunset02.com/sites/default/files/styles/300x300/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514_0.jpg"},
-    // { name: "hahaha", image: "https://beinglike.com/wp-content/uploads/2017/02/Objects-That-Make-Your-Camping-300x300.jpg"},
-    // { name: "Lalala", image: "https://macaobeachhostel.com/wp-content/uploads/2017/08/Slide01-2.jpg"}
-]
+// Schema setup
+var gocampingSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+// Convert to model
+var GoCamping = mongoose.model("GoCamping", gocampingSchema);
+
+// Create a GoCamping
+// GoCamping.create({
+//     name: "Yingyang", 
+//     image: "https://macaobeachhostel.com/wp-content/uploads/2017/08/Slide01-2.jpg"
+    
+// }, (err, gocamping) => {
+//     if(err) { console.log(err) }
+//     else { console.log("Newly created gocamping")
+//             console.log(gocamping) }
+// })
+
+
+// var camps = [
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"},
+//     { name: "Yosemite", image: "https://www.visitwhitemountains.com/UploadedFiles/Vendors/77/DanforthBay-18.jpg"}
+//     // { name: "Hiiiii", image: "https://img.sunset02.com/sites/default/files/styles/300x300/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514_0.jpg"},
+//     // { name: "hahaha", image: "https://beinglike.com/wp-content/uploads/2017/02/Objects-That-Make-Your-Camping-300x300.jpg"},
+//     // { name: "Lalala", image: "https://macaobeachhostel.com/wp-content/uploads/2017/08/Slide01-2.jpg"}
+// ]
 
 
 app.get("/", (req, res) => {
@@ -28,8 +52,14 @@ app.get("/", (req, res) => {
 
 app.get("/campgrounds", (req, res) => {
 
-    res.render("campgrounds", {camps: camps})
-
+    // Get all gocampings from DB
+    GoCamping.find({}, (err, allGoCampings) => {
+        if (err) { console.log(err) }
+        else {
+            res.render("campgrounds", {camps: allGoCampings})
+         }
+    })
+    
 })
 
 app.post("/campgrounds", (req, res) => {
@@ -38,9 +68,15 @@ app.post("/campgrounds", (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
     var newCamp = { name: name, image: image }
-    camps.push(newCamp);
-    // Redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    
+    // Create a new gocamping and save to DB
+    GoCamping.create(newCamp, (err, newlyCreated) =>{
+        if(err) { console.log(err)}
+        else {
+            // Redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    })
 })
 
 app.get("/campgrounds/new", (req, res) => {
